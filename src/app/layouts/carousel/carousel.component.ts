05626d44db5498b7f5ae5e159935423d6516f2bc
Toñi <sanchezmaiquez84@gmail.com>
2024-04-02 16:19:45 +0200

    carousel con mockoon

--- src/app/layouts/carousel/carousel.component.ts
+++ src/app/layouts/carousel/carousel.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Imagen } from './imagen.intercace';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit {
+  imagenes: Imagen[] = [];
 
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit(): void {
+    this.obtenerArticulosRest();
+  }
+
+  private obtenerArticulosRest() {
+    const urlEndPoint: string = "http://localhost:3002/carousel";
+    this.http.get<Imagen[]>(urlEndPoint).subscribe(
+      (data) => {
+        this.imagenes = data;
+      },
+      (error) => {
+        console.error('Error al obtener imágenes:', error);
+      }
+    );
+  }
 }
