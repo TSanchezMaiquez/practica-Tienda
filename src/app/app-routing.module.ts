465f99be08ad0589a932ea9a83d0f9d07bb0742f
Toñi <sanchezmaiquez84@gmail.com>
2024-04-02 15:33:13 +0200

    carousel hecho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TiendaComponent } from './tienda/tienda.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TiendaComponent},
+  {path: 'nsootros', component: NosotrosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { TiendaComponent } from './tienda/tienda.component';
+import { CarouselComponent } from './layouts/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NosotrosComponent,
+    TiendaComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
